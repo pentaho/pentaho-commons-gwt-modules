b8b870c24cce6bc459d7c738893e81736a17a2f6
Nick Baker <nbaker@pentaho.com>
2008-09-03 13:57:09 +0000

    RoundedButton added

--- src/org/pentaho/gwt/widgets/client/Widgets.java
+++ src/org/pentaho/gwt/widgets/client/Widgets.java
@@ -1,9 +1,14 @@
 package org.pentaho.gwt.widgets.client;
 
+import org.pentaho.gwt.widgets.client.buttons.RoundedButton;
 import org.pentaho.gwt.widgets.client.i18n.WidgetsLocalizedMessages;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -16,6 +21,18 @@ public class Widgets implements EntryPoint {
    * This is the entry point method.
    */
   public void onModuleLoad() {
+    final RoundedButton btn = new RoundedButton("Test");
+    RoundedButton btn2 = new RoundedButton("disable", new Command(){
+
+      public void execute() {
+        btn.setEnabled(!btn.isEnabled());
+      }
+      
+    });
+    HorizontalPanel hbox = new HorizontalPanel();
+    hbox.add(btn);
+    hbox.add(btn2);
+    RootPanel.get("canvas").add(hbox);
   }
   
   public static WidgetsLocalizedMessages getLocalizedMessages() {
