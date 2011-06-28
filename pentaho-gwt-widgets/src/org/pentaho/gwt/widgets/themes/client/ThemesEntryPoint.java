cc7a7e8bccc5123c54231a58453af06ad7fa40b3
Nick Baker <nbaker@pentaho.com>
2011-06-28 03:20:03 +0000

    Migrating Themes to common-ui plugin. Adding the ability to switch themes in PUC. Themes can optionally be set to hidden. Optional i18n display names.

---
+++ pentaho-gwt-widgets/src/org/pentaho/gwt/widgets/themes/client/ThemesEntryPoint.java
@@ -0,0 +1,14 @@
+package org.pentaho.gwt.widgets.themes.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * User: nbaker
+ * Date: 6/12/11
+ */
+public class ThemesEntryPoint implements EntryPoint {
+
+  public void onModuleLoad() {
+    
+  }
+}
