d3aac319e5941a200110ff75d21a53705e12dc72
William Seyler <wseyler@pentaho.com>
2011-11-04 18:36:33 +0000

    Changed most of the methods to static so that some methods can be called on the class.

--- pentaho-gwt-widgets/src/org/pentaho/gwt/widgets/client/filechooser/XMLToRepositoryFileTreeConverter.java
+++ pentaho-gwt-widgets/src/org/pentaho/gwt/widgets/client/filechooser/XMLToRepositoryFileTreeConverter.java
@@ -3,12 +3,12 @@ package org.pentaho.gwt.widgets.client.filechooser;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import com.google.gwt.i18n.client.DateTimeFormat;
+
 import com.google.gwt.xml.client.Document;
 import com.google.gwt.xml.client.Element;
 import com.google.gwt.xml.client.Node;
-import com.google.gwt.xml.client.XMLParser;
 import com.google.gwt.xml.client.NodeList;
+import com.google.gwt.xml.client.XMLParser;
 
 
   /**
@@ -33,107 +33,111 @@ import com.google.gwt.xml.client.NodeList;
       return fileTree;
     }
     
-    public List<RepositoryFile> getTrashFiles(String xmlData) {
+    public static final List<RepositoryFile> getTrashFiles(String xmlData) {
+      return getFileListFromXml(xmlData);
+    }
+    
+    public static final List<RepositoryFile> getFileListFromXml(String xmlData) {
       Document doc = getXMLDocumentFromString(xmlData);
-      List<RepositoryFile> trashFiles = new ArrayList<RepositoryFile>();
+      List<RepositoryFile> files = new ArrayList<RepositoryFile>();
       NodeList nodes = doc.getElementsByTagName("repositoryFileDto");
       for (int i=0; i<nodes.getLength(); i++) {
         RepositoryFile trashItem = getRepositoryFileFromXmlElement(nodes.item(i));
-        trashFiles.add(trashItem);
+        files.add(trashItem);
       }
-      return trashFiles;
+      return files;
     }
     
   
-    private final String getName(Element repoFile) {
+    private static final String getName(Element repoFile) {
       return getNodeValueByTagName(repoFile, "name");
     }
     
-    private final String getId(Element repoFile) {
+    private static final String getId(Element repoFile) {
       return getNodeValueByTagName(repoFile, "id");
     }
     
-    private final String getCreatedDate(Element repoFile) {
+    private static final String getCreatedDate(Element repoFile) {
       return getNodeValueByTagName(repoFile, "createdDate");
     }
     
-    private final String getlastModifiedDate(Element repoFile) {
+    private static final String getlastModifiedDate(Element repoFile) {
       return getNodeValueByTagName(repoFile, "lastModifiedDate");
     }
 
-    private final String getFileSize(Element repoFile) {
+    private static final String getFileSize(Element repoFile) {
       return getNodeValueByTagName(repoFile, "fileSize");
     }
     
-    private final String isFolder(Element repoFile) {
+    private static final String isFolder(Element repoFile) {
       return getNodeValueByTagName(repoFile, "folder");
     }
     
-    private final String getPath(Element repoFile) {
+    private static final String getPath(Element repoFile) {
       return getNodeValueByTagName(repoFile, "path");
     }
 
-    private final String isHidden(Element repoFile) {
+    private static final String isHidden(Element repoFile) {
       return getNodeValueByTagName(repoFile, "hidden");
     }
 
-    private final String isVersioned(Element repoFile) {
+    private static final String isVersioned(Element repoFile) {
       return getNodeValueByTagName(repoFile, "versioned");      
     }
 
-    private final String getVersionId(Element repoFile) {
+    private static final String getVersionId(Element repoFile) {
       return getNodeValueByTagName(repoFile, "versionId");      
     }
 
-    private final String isLocked(Element repoFile) {
+    private static final String isLocked(Element repoFile) {
       return getNodeValueByTagName(repoFile, "locked");
     }
 
-    private final String getLockOwner(Element repoFile) {
+    private static final String getLockOwner(Element repoFile) {
       return getNodeValueByTagName(repoFile, "lockOwner");      
     }
 
-    private final String getLockMessage(Element repoFile) {
+    private static final String getLockMessage(Element repoFile) {
       return getNodeValueByTagName(repoFile, "lockMessage");      
     }
 
-    private final String getLockDate(Element repoFile) {
+    private static final String getLockDate(Element repoFile) {
       return getNodeValueByTagName(repoFile, "lockDate");
     }
 
-    private final String getOwner(Element repoFile) {
+    private static final String getOwner(Element repoFile) {
       return getNodeValueByTagName(repoFile, "owner");
     }
 
-    private final String getOwnerType(Element repoFile) { 
+    private static final String getOwnerType(Element repoFile) { 
       return getNodeValueByTagName(repoFile, "ownerType");
     }
     
-    private final String getTitle(Element repoFile) {
+    private static final String getTitle(Element repoFile) {
       return getNodeValueByTagName(repoFile, "title");
     }
 
-    private final String getDescription(Element repoFile) { 
+    private static final String getDescription(Element repoFile) { 
       return getNodeValueByTagName(repoFile, "description");
     }
 
-    private final String getLocale(Element repoFile) {
+    private static final String getLocale(Element repoFile) {
       return getNodeValueByTagName(repoFile, "locale");
     }
 
-    private final String getOriginalParentFolderPath(Element repoFile) { 
+    private static final String getOriginalParentFolderPath(Element repoFile) { 
       return getNodeValueByTagName(repoFile, "originalParentFolderPath");
     }
 
-    private final String getOriginalParentFolderId(Element repoFile) {
+    private static final String getOriginalParentFolderId(Element repoFile) {
       return getNodeValueByTagName(repoFile, "originalParentFolderId");
     }
 
-    private final String getDeletedDate(Element repoFile) {
+    private static final String getDeletedDate(Element repoFile) {
       return getNodeValueByTagName(repoFile, "deletedDate");
     }
 
-    private Date parseDateTime(String dateTimeString) { 
+    private static final Date parseDateTime(String dateTimeString) { 
       // parse the date
       Date date = null;
         try {
@@ -145,7 +149,7 @@ import com.google.gwt.xml.client.NodeList;
       return date;     
     }
     
-    private RepositoryFile getRepositoryFileFromXmlElement(Node repoFileNode) {
+    private static RepositoryFile getRepositoryFileFromXmlElement(Node repoFileNode) {
       Element repoFileElement = (Element) repoFileNode;
       RepositoryFile repoFile = new RepositoryFile();
       repoFile.setName(getName(repoFileElement));
@@ -192,14 +196,14 @@ import com.google.gwt.xml.client.NodeList;
       return value;
     }
     
-    private Document getXMLDocumentFromString(String xmlText) {
+    private static Document getXMLDocumentFromString(String xmlText) {
       return (Document) XMLParser.parse(xmlText);
     }
 
     /*
      * Get Node Value of the element matching the tag name
      */
-    private String getNodeValueByTagName(Element element, String tagName) {
+    private static final String getNodeValueByTagName(Element element, String tagName) {
       Node node = getNodeByTagName(element, tagName);
       if(node != null && node.getFirstChild() != null) {
         return node.getFirstChild().getNodeValue();
@@ -207,11 +211,12 @@ import com.google.gwt.xml.client.NodeList;
         return null;
       }
     }
-    private Node getFileNode(Element element) {
+    
+    private static final Node getFileNode(Element element) {
       return getNodeByTagName(element, "file");
     }
     
-    private List<Node> getChildrenNodes(Element element) {
+    private static final List<Node> getChildrenNodes(Element element) {
       return getNodesByTagName(element, "children");
     }
     
@@ -219,7 +224,7 @@ import com.google.gwt.xml.client.NodeList;
      * Return the first name that matched the tagName. Starting from the 
      * current element location
      */
-    private Node getNodeByTagName(Element element, String tagName) {
+    private static final Node getNodeByTagName(Element element, String tagName) {
       NodeList list = element.getChildNodes();
       for(int i=0;i<list.getLength();i++) {
         Node node = list.item(i);
@@ -234,7 +239,7 @@ import com.google.gwt.xml.client.NodeList;
      * Return all node matching the tagName, starting from the current element
      * location
      */
-    private List<Node> getNodesByTagName(Element element, String tagName) {
+    private static final List<Node> getNodesByTagName(Element element, String tagName) {
       List<Node> nodes = new ArrayList<Node>();
       NodeList list = element.getChildNodes();
       for(int i=0;i<list.getLength();i++) {
