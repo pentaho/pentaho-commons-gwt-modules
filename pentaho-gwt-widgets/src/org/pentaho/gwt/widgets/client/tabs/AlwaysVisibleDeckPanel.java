3950fe8f018fce4277bb051082649da66e95c68c
Nick Baker <nbaker@pentaho.com>
2013-02-20 15:22:33 -0500

    [ANALYZER-1520] Fixing issue with reusing elements, removed some commented code

--- pentaho-gwt-widgets/src/org/pentaho/gwt/widgets/client/tabs/AlwaysVisibleDeckPanel.java
+++ pentaho-gwt-widgets/src/org/pentaho/gwt/widgets/client/tabs/AlwaysVisibleDeckPanel.java
@@ -21,16 +21,13 @@ public class AlwaysVisibleDeckPanel extends DeckPanel {
     Widget oldWidget = curWidget;
     curWidget = getWidget(index);
 
-    if (curWidget != oldWidget) {
-      if(oldWidget != null){
-        moveOffscreen(oldWidget);
-      }
-
-
-      curWidget.getElement().getParentElement().getStyle().setProperty("position", "relative");
-      curWidget.getElement().getParentElement().getStyle().setProperty("left", "0");
-
+    if (curWidget != oldWidget && oldWidget != null){
+      moveOffscreen(oldWidget);
     }
+
+    curWidget.getElement().getParentElement().getStyle().setProperty("position", "relative");
+    curWidget.getElement().getParentElement().getStyle().setProperty("left", "0");
+
   }
 
 
@@ -54,47 +51,4 @@ public class AlwaysVisibleDeckPanel extends DeckPanel {
     w.getElement().getParentElement().getStyle().setProperty("display", "");
     moveOffscreen(w);
   }
-
-  //
-//  @Override
-//  public void showWidget(int index) {
-//    Widget widgetToShow = this.getWidget(index);
-//    int curIdx = this.getVisibleWidget();
-//    if(curIdx > -1){
-//      Widget currentWidget = this.getWidget(this.getVisibleWidget());
-//      DOM.setStyleAttribute(currentWidget.getParent().getElement(), "left", "-5000px");
-//    }
-//    DOM.setStyleAttribute(widgetToShow.getParent().getElement(), "left", "0px");
-//    curWidget = widgetToShow;
-//  }
-//
-//  @Override
-//  public int getVisibleWidget() {
-//    return this.getWidgetIndex(curWidget);
-//  }
-//
-//  private Element createWidgetWrapper() {
-//    Element div = DOM.createDiv();
-//    div.getStyle().setProperty("width", "100%");
-//    div.getStyle().setProperty("height", "0px");
-//    div.getStyle().setProperty("padding", "0px");
-//    div.getStyle().setProperty("margin", "0px");
-//    return div;
-//  }
-//
-//  @Override
-//  public void add(Widget w) {
-//
-//    Element wrapper = createWidgetWrapper();
-//    getElement().appendChild(wrapper);
-//
-//    super.add(w, wrapper);
-//
-//    DOM.setStyleAttribute(wrapper, "height", "100%");
-//    DOM.setStyleAttribute(wrapper, "position", "absolute");
-//    DOM.setStyleAttribute(wrapper, "zIndex", "12345");
-//    w.setSize("100%", "100%");
-//    DOM.setStyleAttribute(wrapper, "height", "100%");
-//
-//  }
 }
