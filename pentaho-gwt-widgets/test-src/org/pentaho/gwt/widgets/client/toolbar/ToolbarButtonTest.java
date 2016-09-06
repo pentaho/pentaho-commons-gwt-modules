a680b5c9d6af3132cadef01b07f2178d27dd035b
Aliaksandr Pratkin <aliaksandr_pratkin@epam.com>
2016-09-06 11:34:55 +0300

    [BISERVER-12933] - Tooltip and shape visible of buttons 'Edit Content', 'Save', 'Save As'

--- pentaho-gwt-widgets/test-src/org/pentaho/gwt/widgets/client/toolbar/ToolbarButtonTest.java
+++ pentaho-gwt-widgets/test-src/org/pentaho/gwt/widgets/client/toolbar/ToolbarButtonTest.java
@@ -12,7 +12,7 @@
 * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 * See the GNU Lesser General Public License for more details.
 *
-* Copyright (c) 2002-2015 Pentaho Corporation..  All rights reserved.
+* Copyright (c) 2002-2016 Pentaho Corporation..  All rights reserved.
 */
 
 package org.pentaho.gwt.widgets.client.toolbar;
@@ -94,6 +94,7 @@ public class ToolbarButtonTest {
     button.currentImage = currentImage;
     final Image image = mock( Image.class );
     button.image = image;
+    button.eventWrapper = mock( FocusPanel.class );
 
     button.setEnabled( true );
     verify( button.button ).removeStyleName( contains( "disabled" ) );
