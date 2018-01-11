a699ade1ff4a08abb4a5d68f4ca7f7544ff13613
Bruno Cavaleiro <befcavaleiro@gmail.com>
2018-01-11 15:53:34 +0000

    [BACKLOG-19513] Add logic to redirect to /Home instead of /Login when SSO is enabled (#651)
    * [BACKLOG-19513] Add logic to redirect to /Home instead of /Login when SSO is enabled.
    
    * [BACKLOG-19513] Add logic to redirect to /Home instead of /Login when SSO is enabled.
    
    * [BACKLOG-19513] Add logic to redirect to /Home instead of /Login when SSO is enabled.

--- widgets/src/main/java/org/pentaho/gwt/widgets/client/dialogs/SessionExpiredDialog.java
+++ widgets/src/main/java/org/pentaho/gwt/widgets/client/dialogs/SessionExpiredDialog.java
@@ -59,8 +59,12 @@ public class SessionExpiredDialog extends PromptDialogBox {
       if (event.target === element) {
         event.stopPropagation();
         var pathArray = $wnd.location.pathname.split( '/' );
+        var redirect = 'Login'
+        if ( this.ssoEnabled !== 'undefined' && this.ssoEnabled ) {
+          redirect = 'Home'
+        }
         if ( null != pathArray && pathArray.length > 1 ) {
-          $wnd.location.assign( '/' + pathArray[ 1 ] + '/Login' );
+          $wnd.location.assign( '/' + pathArray[ 1 ] + '/' + redirect );
         }
       }
     };
