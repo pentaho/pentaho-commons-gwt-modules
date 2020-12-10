0b2251aa2ca3a6dc69acf4b6f26844e73dbd55c9
ssamora <sergiosamora@gmail.com>
2020-12-10 19:43:45 +0000

    [BISERVER-14495] Schedule Information across timezone is shown incorrectly when the recurrence is Weekly.

--- widgets/src/test/java/org/pentaho/gwt/widgets/client/utils/TimeUtilTest.java
+++ widgets/src/test/java/org/pentaho/gwt/widgets/client/utils/TimeUtilTest.java
@@ -21,6 +21,7 @@ import com.google.gwtmockito.GwtMockitoTestRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+
 import static org.junit.Assert.assertEquals;
 
 @RunWith( GwtMockitoTestRunner.class )
@@ -47,7 +48,7 @@ public class TimeUtilTest {
     assertEquals( -1, dayVariance );
 
     dateTimeFormat = "2020-11-05T05:30:00-05:30";
-    dayVariance = TimeUtil.getDayVariance( 5, 29, dateTimeFormat );
+    dayVariance = TimeUtil.getDayVariance( 6, 29, dateTimeFormat );
     assertEquals( 0, dayVariance );
 
     dateTimeFormat = "2020-11-05T05:30:00-05:30";
@@ -72,5 +73,51 @@ public class TimeUtilTest {
   public void testGetDayOfWeek_previous() {
     int previousDayOfWeek = TimeUtil.getDayOfWeek( TimeUtil.DayOfWeek.MON, -1 );
     assertEquals( TimeUtil.DayOfWeek.SUN.ordinal(), previousDayOfWeek );
+  }
+
+  @Test
+  public void getTargetOffsetFromTimezoneStringTest() {
+    double timeOffset = TimeUtil.getTargetOffsetFromTimezoneString( "Eastern Daylight Time (UTC-0500)" );
+    assertEquals( -5.0, timeOffset, 0 );
+    timeOffset = TimeUtil.getTargetOffsetFromTimezoneString( "Atlantic Daylight Time (UTC-400)" );
+    assertEquals( -4.0, timeOffset, 0 );
+    timeOffset = TimeUtil.getTargetOffsetFromTimezoneString( "India Daylight Time (UTC+0530)" );
+    assertEquals( 5.5, timeOffset, 0 );
+    timeOffset = TimeUtil.getTargetOffsetFromTimezoneString( "Nepal Summer Time (UTC+0545)" );
+    assertEquals( 5.75, timeOffset, 0 );
+  }
+
+  @Test
+  public void getTargetOffsetFromTimezoneStringInvalidValuesTest() {
+    double timeOffset = TimeUtil.getTargetOffsetFromTimezoneString( "Eastern Daylight Time (UTC0500)" );
+    assertEquals( 0.0, timeOffset, 0 );
+    timeOffset = TimeUtil.getTargetOffsetFromTimezoneString( "Eastern Daylight Time (UTC--0500)" );
+    assertEquals( 0.0, timeOffset, 0 );
+    timeOffset = TimeUtil.getTargetOffsetFromTimezoneString( "2020-12-01T10:30:00-05:45" );
+    assertEquals( 0.0, timeOffset, 0 );
+  }
+
+  @Test
+  public void getTargetOffsetFromDateTimeStringTest() {
+    double timeOffset = TimeUtil.getTargetOffsetFromDatetimeString( "2018-02-27T07:30:00-05:00" );
+    assertEquals( -5.0, timeOffset, 0 );
+    timeOffset = TimeUtil.getTargetOffsetFromDatetimeString( "2020-12-01T10:30:00-05:45" );
+    assertEquals( -5.75, timeOffset, 0 );
+    timeOffset = TimeUtil.getTargetOffsetFromDatetimeString( "2020-12-01 10:30:00+01:00" );
+    assertEquals( 1.0, timeOffset, 0 );
+    timeOffset = TimeUtil.getTargetOffsetFromDatetimeString( "2020-12-01 10:30:00+11:30" );
+    assertEquals( 11.5, timeOffset, 0 );
+  }
+
+  @Test
+  public void getTargetOffsetFromDateTimeStringInvalidValuesTest() {
+    double timeOffset = TimeUtil.getTargetOffsetFromDatetimeString( "2018-02-27X07:30:00-05:00" );
+    assertEquals( 0.0, timeOffset, 0 );
+    timeOffset = TimeUtil.getTargetOffsetFromDatetimeString( "2020-12T10:30:00-05:45" );
+    assertEquals( 0.0, timeOffset, 0 );
+    timeOffset = TimeUtil.getTargetOffsetFromDatetimeString( "2020-12-01 10:30:00+01" );
+    assertEquals( 0.0, timeOffset, 0 );
+    timeOffset = TimeUtil.getTargetOffsetFromDatetimeString( "Eastern Daylight Time (UTC-0500)" );
+    assertEquals( 0.0, timeOffset, 0 );
   }
 }
