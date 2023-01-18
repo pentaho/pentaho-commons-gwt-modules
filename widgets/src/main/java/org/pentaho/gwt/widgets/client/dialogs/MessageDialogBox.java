3ca5fa17cc5a381fd61a1ef5bdcba2d7e074d7f9
Duarte Cunha Leão <duarte.leao@gmail.com>
2023-01-18 17:03:56 +0000

    [BACKLOG-36964] Added ARIA roles support to base GWT Dialog classes
    - DialogBox, PromptDialogBox, MessageDialogBox
    - Added convenience constructor overloads to MessageDialogBox, to facilitate its use, in places where PromptDialogBox is currently being incorrectly used.
    - Added new classes: HorizontalFlexPanel and VerticalFlexPanel, in-place replacements for `HorizontalPanel` and `VerticalPanel`. Already contain appropriate ARIA role. Will add their *flexible* layout features on a subsequent commit.

--- widgets/src/main/java/org/pentaho/gwt/widgets/client/dialogs/MessageDialogBox.java
+++ widgets/src/main/java/org/pentaho/gwt/widgets/client/dialogs/MessageDialogBox.java
@@ -12,28 +12,88 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- * Copyright (c) 2002-2017 Hitachi Vantara..  All rights reserved.
+ * Copyright (c) 2002-2023 Hitachi Vantara..  All rights reserved.
  */
 
 package org.pentaho.gwt.widgets.client.dialogs;
 
+import com.google.gwt.aria.client.Roles;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Widget;
 import org.pentaho.gwt.widgets.client.messages.Messages;
 
+import static org.pentaho.gwt.widgets.client.utils.ElementUtils.ensureId;
+
+/**
+ * The <code>MessageDialogBox</code> class is a dialog box intended to display text messages.
+ * <p>
+ *   By default, the ARIA role is <code>alertdialog</code>.
+ * </p>
+ * <p>
+ *   By default, the dialog's {@link #getAriaDescribedBy()} is set to the identifier of the dialog's content widget.
+ *   A different element can be used, by calling {@link #setAriaDescribedBy(String)},
+ *   when the content is more complex than a single text message.
+ * </p>
+ */
 public class MessageDialogBox extends PromptDialogBox {
 
-  public MessageDialogBox( String title, String message, boolean isHTML, boolean autoHide, boolean modal ) {
-    super( title, Messages.getString( "dialog.button.ok" ), null, autoHide, modal, isHTML ? new HTML( message ) : new Label( message ) ); //$NON-NLS-1$
+  public MessageDialogBox( String title, String message, boolean isHTML ) {
+    this( title, message, isHTML, false, true );
+  }
+
+  public MessageDialogBox( String title, Widget messageContent ) {
+    this( title, messageContent, null );
+  }
+
+  public MessageDialogBox( String title, String message, boolean isHTML, String okText ) {
+    this( title, message, isHTML, false, true, okText );
+  }
+
+  public MessageDialogBox( String title, Widget messageContent, String okText ) {
+    this( title, messageContent, okText, null );
+  }
+
+  public MessageDialogBox( String title, String message, boolean isHTML, String okText, String cancelText ) {
+    this( title, message, isHTML, okText, null, cancelText );
+  }
+
+  public MessageDialogBox( String title, Widget messageContent, String okText, String cancelText ) {
+    this( title, messageContent, okText, null, cancelText );
   }
 
   public MessageDialogBox( String title, String message, boolean isHTML,
-                           boolean autoHide, boolean modal, String okText ) {
-    super( title, okText, null, autoHide, modal, isHTML ? new HTML( message ) : new Label( message ) ); //$NON-NLS-1$
+                           String okText, String notOkText, String cancelText ) {
+    this( title, message, isHTML, false, true, okText, notOkText, cancelText );
+  }
+
+  public MessageDialogBox( String title, Widget messageContent, String okText, String notOkText, String cancelText ) {
+    this( title, messageContent, false, true, okText, notOkText, cancelText );
+  }
+
+  public MessageDialogBox( String title, String message, boolean isHTML, boolean autoHide, boolean modal ) {
+    this( title, message, isHTML, autoHide, modal, Messages.getString( "dialog.button.ok" ), null, null );
+  }
+
+  public MessageDialogBox( String title, String message, boolean isHTML, boolean autoHide, boolean modal,
+                           String okText ) {
+    this( title, message, isHTML, autoHide, modal, okText, null, null );
   }
 
   public MessageDialogBox( String title, String message, boolean isHTML, boolean autoHide, boolean modal,
       String okText, String notOkText, String cancelText ) {
-    super( title, okText, notOkText, cancelText, autoHide, modal, isHTML ? new HTML( message ) : new Label( message ) ); //$NON-NLS-1$
+    this(title, isHTML ? new HTML( message ) : new Label( message ), autoHide, modal, okText, notOkText, cancelText );
+  }
+
+  public MessageDialogBox( String title, Widget messageContent, boolean autoHide, boolean modal,
+                           String okText, String notOkText, String cancelText ) {
+    super( title, okText, notOkText, cancelText, autoHide, modal, messageContent );
+
+    // ARIA
+    // Override role from "dialog" to "alertdialog".
+    setAriaRole( Roles.getAlertdialogRole() );
+
+    // aria-describedby
+    setAriaDescribedBy( ensureId( getContent() ) );
   }
 }
