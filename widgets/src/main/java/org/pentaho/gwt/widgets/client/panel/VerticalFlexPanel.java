e226fe71d6ceb05fd93f1f7466c56f6eeb0ac9c1
Duarte Cunha Leão <duarte.leao@gmail.com>
2023-01-30 16:15:15 +0000

    [BACKLOG-37004] Added flex layout CSS styles for HorizontalFlexPanel and VerticalFlexPanel

--- widgets/src/main/java/org/pentaho/gwt/widgets/client/panel/VerticalFlexPanel.java
+++ widgets/src/main/java/org/pentaho/gwt/widgets/client/panel/VerticalFlexPanel.java
@@ -20,6 +20,19 @@ package org.pentaho.gwt.widgets.client.panel;
 import com.google.gwt.aria.client.Roles;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
+/**
+ * A flexible version of the framework class {@link VerticalPanel}.
+ * <p>
+ * To enable the flexible behavior, the panel must be rendered within an HTML container
+ * which has the <code>responsive</code> CSS class set.
+ * This allows for the panel to be used in base classes and for its flexible behavior only taking effect
+ * when the subclass opts into it. When not enabled, the panel is fully backwards compatible.
+ * This allows for incremental development.
+ * For example, the <code>PromptDialogBox</code> class may use this flexible panel, however,
+ * its flexible behavior only takes effect when the dialog is marked responsive,
+ * by calling its <code>PromptDialogBox#setResponsive(boolean)</code> method.
+ * </p>
+ */
 public class VerticalFlexPanel extends VerticalPanel {
   public VerticalFlexPanel() {
     addStyleName( "gwt-v-panel" );
