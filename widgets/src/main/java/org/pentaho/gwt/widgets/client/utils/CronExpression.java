8e18bf55e2fd690c3334437d21675b5b51e029d4
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-09-29 14:57:59 -0400

    HNC-766: updated license and copyright headers

--- widgets/src/main/java/org/pentaho/gwt/widgets/client/utils/CronExpression.java
+++ widgets/src/main/java/org/pentaho/gwt/widgets/client/utils/CronExpression.java
@@ -1,30 +1,15 @@
-/*
- ***********************************************************************************************************************
+/*! ******************************************************************************
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * Pentaho
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file.
  *
- * This file is a copy from the Quartz project - http://quartz-scheduler.org/ from version 1.5.1
- * with the following changes:
- *   a- Removed all non-GWT friendly classes (Locale, Calendar, StringTokenizer, ObjectOutputStream, etc.
- *   b- Re-implemented the buildExpression method using split instead of StringTokenizer
- *   c- Externalized all strings to the i18n package
- *   d- Code-formatted to meet Hitachi Vantara standards
- *   e- Removed extraneous helper methods not needed for cron validation
- *
- * These changes were expressly made to allow GWT compilation (read Javascript translation) for validation of
- * Quartz-specific cron expressions. All other comments and attributions remain intact.
- *
- * Copyright 2012 - 2017 Hitachi Vantara.  All rights reserved.
- *
- ***********************************************************************************************************************
- */
+ * Change Date: 2028-08-13
+ ******************************************************************************/
+
 
 package org.pentaho.gwt.widgets.client.utils;
 
