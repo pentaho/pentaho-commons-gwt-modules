a40584acbdd19b2f2b54b51e68ab30ca00a6e62a
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-11-11 13:37:32 -0500

    DEVO-11194: updated license and copyright headers

--- widgets/src/main/java/org/pentaho/mantle/client/csrf/IConsumer.java
+++ widgets/src/main/java/org/pentaho/mantle/client/csrf/IConsumer.java
@@ -1,3 +1,14 @@
+/*! ******************************************************************************
+ *
+ * Pentaho
+ *
+ * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file.
+ *
+ * Change Date: 2029-07-20
+ ******************************************************************************/
 package org.pentaho.mantle.client.csrf;
 
 // Lambda expressions, and Java 8 types are only supported from GWT 2.8 onwards...
